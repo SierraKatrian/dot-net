a27fffc6df34462c4b629d53c0bc9d0d6479466f
SierraKatrian <sierra@sierrakatrian.com>
2017-01-25 21:15:59 -0500

    Added completed Assignment1

---
+++ Assignment1/Assignment1/Controllers/HumberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class HumberController : Controller
+    {
+        // GET: Humber
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            Students sierra = new Students();
+            sierra.Name = "Sierra Katrian";
+            sierra.StudentNum = "N01169395";
+            sierra.Program = "Web Developement";
+
+            return View(sierra);
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class Students
+    {
+        public string Name { get; set; }
+        public string StudentNum { get; set; }
+        public string Program { get; set; }
+
+    }
+}
